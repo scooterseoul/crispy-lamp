a9ff116843b6970e010a5e63e507a2ec61d72e0b
Claudette Alexander <claudette.alexander@developersinstitute.co.nz>
2023-03-12 22:21:46 +0900

    css

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,21 @@
-import "./App.css";
+import styles from "./App.module.css";
+import { Routes, Route } from "react-router-dom";
+import ProductList from "./components/AllProducts";
+import Header from "./components/Header";
+import mainpic from "./images/mainpic.png";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      {/* <div>
+        <img src={mainpic} className={styles.mainpic}></img>
+      </div> */}
+      <Routes>
+        <Route path="/" element={<ProductList />} />
+      </Routes>
+    </>
   );
-}
+};
 
 export default App;
