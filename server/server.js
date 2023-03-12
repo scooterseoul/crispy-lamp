9e797ad3d83ffe03d1cc34a0ca9e3a2a15f7c0d2
Claudette Alexander <claudette.alexander@developersinstitute.co.nz>
2023-03-12 14:17:16 +0900

    Clientside

---
+++ server/server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const productsRoutes = require("./product/routes");
+const app = express();
+const port = 5000;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.use("/api/v1/products", productsRoutes);
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+});
